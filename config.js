276ca02296c4a4798e8d198d4962796ab83eec49
LCH2988 <80178918+LCH2988@users.noreply.github.com>
2025-02-08 14:42:14 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // config.js
 const CONFIG = {
     LIFF_ID: '2001679903-r5VXNe5g',  // 請替換為您的 LIFF ID
-    API_URL: 'https://script.google.com/macros/s/YOUR_DEPLOYMENT_ID/exec',  // 請替換為您的 Apps Script 部署網址
+    API_URL: 'https://script.google.com/macros/s/AKfycbwl5Jl-j8IfO2yqLuBDf-q5S206yrmmjzsTtien65v3MpWNNGLcgALkQQeiEob9MspvpQ/exec',  // 請替換為您的 Apps Script 部署網址
     DEBUG: true  // 開發時設為 true，正式環境設為 false
 };
